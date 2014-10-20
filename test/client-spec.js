f4292cc40482fc7398bc13c30dd361436a1429c6
Peter de Croos <cultofmetatron@aumlogic.com>
2014-10-19 21:48:59 -0700

    added stubbs for the top delcarations

---
+++ test/client-spec.js
@@ -0,0 +1,25 @@
+var mocha = require('mocha');
+var should = require('should');
+var path = require('path');
+var dotenv = require('dotenv');
+dotenv._getKeysAndValuesFromEnvFilePath(path.join(__dirname, '..', '.env'));
+dotenv._setEnvs
+
+
+describe('client', function() {
+
+  it('will authenticate with the token', function(done) {
+    console.log(process.env.MINDBODY_SOURCE);
+  
+  
+  });
+
+
+
+
+
+})
+
+
+
+
---
+++ test/mb-class.js
@@ -0,0 +1,7 @@
+var mocha = require('mocha');
+var should = require('should');
+var path = require('path');
+var dotenv = require('dotenv');
+dotenv._getKeysAndValuesFromEnvFilePath(path.join(__dirname, '..', '.env'));
+dotenv._setEnvs
+
